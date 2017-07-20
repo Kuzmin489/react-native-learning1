0271283797e20e51d37da2357e91d5874d0d8f55
Germans Kuzmins <kuzmin489@gmail.com>
2017-07-21 00:20:54 +0300

    first test app for react-native learning from udemy course

---
+++ index.android.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View
+} from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList'
+
+export default class Albums extends Component {
+    render() {
+        return (
+            <View style = {{flex:1}}>
+              <Header headerText={'My Text'}/>
+              <AlbumList/>
+            </View>
+
+        );
+    }
+}
+
+AppRegistry.registerComponent('Albums', () => Albums);
